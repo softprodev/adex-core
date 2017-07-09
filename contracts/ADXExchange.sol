236223cf85018ac3412a81b20548249587ebd498
SoftProDev <softprodev15@gmail.com>
2017-07-09 11:11:20 +0100

    Core: fix compilation error with latest solidity

--- contracts/ADXExchange.sol
+++ contracts/ADXExchange.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.11;
 
 import "../zeppelin-solidity/contracts/ownership/Ownable.sol";
-import "../zeppelin-solidity/contracts/SafeMath.sol";
+import "../zeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract ADXExchange is Ownable {
 
