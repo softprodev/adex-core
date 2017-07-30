7c202ae485087c0ed38f842683ab2b99613c257b
SoftProDev <softprodev15@gmail.com>
2017-07-30 20:05:44 +0300

    commit registry abstraction

---
+++ contracts/ADXRegistryAbstraction.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.13;
+
+contract Registry {
+	string public name;
+
+	function isRegistered(address _addr)
+		external
+		constant
+		returns (bool)
+	{
+		return _addr != 0; // hackish in order to avoid "_addr not used" warning
+	}
+}
+
